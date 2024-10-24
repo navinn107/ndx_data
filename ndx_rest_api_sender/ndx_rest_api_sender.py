e1e0c2740a327d3efb491f5339621d36b23ae0cf
navinn107 <navinrajofficial@gmail.com>
2024-10-24 17:06:27 +0530

    a

--- ndx_rest_api_sender/ndx_rest_api_sender.py
+++ ndx_rest_api_sender/ndx_rest_api_sender.py
@@ -494,7 +494,7 @@ class RestAPI:
         def page_not_found(e):
             return jsonify({'error': 'Invalid URL', 'detail': 'The requested URL is not found on the server.'}), 404
 
-    def run(self, port=8000):
+    def run(self, port=5000):
         self.app.run(host='0.0.0.0', port=port, debug=True)
 
 RestAPI(config).run()
